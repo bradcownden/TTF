a85b00cdec71481b69364e0cbd6bffc6f00f8a2c
Brad Cownden <cowndenb@gmail.com>
2015-12-03 17:23:15 -0600

    Comparison of direct difference as opposed to percent difference

--- comparison.py
+++ comparison.py
@@ -19,27 +19,52 @@ import matplotlib.pyplot as mp
 
 Xme = np.loadtxt("d3X.dat",ndmin=2)
 Xme.sort(axis=1)
-ind = np.lexsort((Xme[:,4],Xme[:,3],Xme[:,2],Xme[:,1]))
-Xmeprime = Xme[ind]
+indx = np.lexsort((Xme[:,4],Xme[:,3],Xme[:,2],Xme[:,1]))
+Xmeprime = Xme[indx]
 
 Xnils = np.loadtxt("NilsData/X_30.dat",ndmin=2)
 Xnils.sort(axis=1)
 
-comp = np.zeros(Xme.shape[0])
+compx = np.zeros(Xmeprime.shape[0])
 
 for i in range(Xmeprime.shape[0]):
-    comp[i] = (Xmeprime[i][0] - Xnils[i][0])*100/Xmeprime[i][0]
+    compx[i] = (Xmeprime[i][0] - Xnils[i][0])
+
+###########################################################
     
-print(comp[0])
-print(comp[1])
+Yme = np.loadtxt("d3Y.dat",ndmin=2)
+Yme.sort(axis=1)
+indy = np.lexsort((Yme[:,4],Yme[:,3],Yme[:,2],Yme[:,1]))
+Ymeprime = Yme[indy]
+
+Ynils = np.loadtxt("NilsData/Y_30.dat",ndmin=2)
+Ynils.sort(axis=1)
+
+compy = np.zeros(Ymeprime.shape[0])
+
+for i in range(Ymeprime.shape[0]):
+    compy[i] = (Ymeprime[i][0] - Ynils[i][0])
+        
     
+###########################################################
+###########################################################    
 
 # Plot the results
 
+mp.figure(1)
+
+mp.subplot(211)
 x = range(Xmeprime.shape[0])
-mp.plot(x,comp,marker=".")
-mp.ylabel('Percent Difference in X Coefficients')
+mp.plot(x,compx,'bo')
+mp.ylabel('Difference in X Coefficients')
 mp.xlabel('Index Number')
 mp.title('X Coefficients Comparison')
+
+mp.subplot(212)
+x = range(Ymeprime.shape[0])
+mp.plot(x,compy,'ro')
+mp.ylabel('Difference in Y Coefficients')
+mp.xlabel('Index Number')
+mp.title('Y Coefficients Comparison')
 mp.show()
     
