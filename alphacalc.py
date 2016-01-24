3918caee8f7aba8afd99ef093f6dbdf039b049e4
Brad Cownden <cowndenb@gmail.com>
2016-01-24 18:49:46 +0800

    Functions added to extract X,Y,S values given i,j,k,l indices from read-in files

--- alphacalc.py
+++ alphacalc.py
@@ -21,7 +21,7 @@ from sympy.abc import t
 
 S = np.genfromtxt("d3S_L10.dat")
 X = np.genfromtxt("d3X_L10.dat")
-Y = np.genfromtxt("d3X_L10.dat")
+Y = np.genfromtxt("d3Y_L10.dat")
 
 
 ###################################################################
@@ -62,10 +62,38 @@ def V(i,j,d):
     return quad(lambda x: (ep(i,d)(x))*(ep(j,d)(x))*math.sin(x)*math.cos(x),0,math.pi/2.)[0]
     
 def T(i,d):
-    return ((w(i,d)**2)*(X[i][1])/2. + 3*(Y[i][1])/2. + 2*(w(i,d)**4)*W00(i,i,i,i,d) + 2*(w(i,d)**2)*W10(i,i,i,i,d) \
+    return ((w(i,d)**2)*(Xval(i,i,i,i))/2. + 3*(Yval(i,i,i,i))/2. + 2*(w(i,d)**4)*W00(i,i,i,i,d) + 2*(w(i,d)**2)*W10(i,i,i,i,d) \
     - (w(i,d)**2)*(A(i,i,d) + (w(i,d)**2)*V(i,i,d)))
-        
-print(T(1,3))
+            
+def Xval(i,j,k,l):
+    temp = sorted([i,j,k,l],reverse=True)
+    for i in range(X.shape[0]):
+        if X[i][0]==temp[0] and X[i][1]==temp[1] and X[i][2]==temp[2] and X[i][3]==temp[3]:
+            return X[i][4]
+            break
+
+def Yval(i,j,k,l):
+    temp = sorted([k,l],reverse=True)
+    for row in range(Y.shape[0]):
+        if Y[row][0]==i and Y[row][1]==j and Y[row][2]==temp[0] and Y[row][3]==temp[1]:
+            return Y[row][4]
+            break
+    print("Y[%d][%d][%d][%d] not found" % (i,j,temp[0],temp[1]))
+
+def Sval(i,j,k,l):
+    for row in range(S.shape[0]):
+        if S[row][0]==i and S[row][1]==j and S[row][2]==k and S[row][3]==l:
+            return S[row][4]
+            break
+    print("S[%d][%d][%d][%d] not found" % (i,j,k,l))
+                    
+
+print("X[2][1][0][0] =", Xval(1,2,0,0))
+print("Y[1][2][0][2] =", Yval(1,2,0,2))
+print("S[0][0][1][0] =", Sval(0,0,1,0))
+    
+    
+
 
     
 
