39e2af3bfe749919c6f2d7976e201778ea437571
Brad Cownden <cowndenb@gmail.com>
2016-04-07 16:14:40 +0800

    Updated loop for optimization. Now achieving agreement across multiple optimization methods

--- jitalphacalc.py
+++ jitalphacalc.py
@@ -10,15 +10,15 @@ quasistable regime using the recursion coefficients. Test up to L=10.
 """
 
 import numpy as np
-from scipy.integrate import quad, dblquad
-from scipy.special import gamma
-from scipy.special import eval_jacobi
+#from scipy.integrate import quad, dblquad
+#from scipy.special import gamma
+#from scipy.special import eval_jacobi
 import math
-from sympy import lambdify, jacobi
-from sympy.functions.elementary.trigonometric import cos as symcos
-from sympy.core import diff
-from sympy.abc import t
-from scipy.optimize import newton_krylov
+#from sympy import lambdify, jacobi
+#from sympy.functions.elementary.trigonometric import cos as symcos
+#from sympy.core import diff
+#from sympy.abc import t
+from scipy.optimize import newton_krylov, diagbroyden, fsolve, root
 import time
 #from array import array
 from numba import jit
@@ -32,7 +32,7 @@ from numba import jit
 Read in values for X,Y,S from recursion relations.
 """
 
-#S = np.genfromtxt("d3S_L10.dat",dtype=np.float)
+S = np.genfromtxt("d3S_L10.dat",dtype=np.float)
 #X = np.genfromtxt("d3X_L10.dat")
 #Y = np.genfromtxt("d3Y_L10.dat")
 
@@ -40,14 +40,14 @@ Read in values for X,Y,S from recursion relations.
 Read in T,R from Mathematica output
 """
 
-#R = np.genfromtxt("Mathematica_R.dat",dtype=np.float)
-#T = np.genfromtxt("Mathematica_T.dat",dtype=np.float)
+R = np.genfromtxt("Mathematica_R.dat",dtype=np.float)
+T = np.genfromtxt("Mathematica_T.dat",dtype=np.float)
 
 
 """
 Read in S,R,T values from binary files
 """
-
+"""
 Ttype = np.dtype([('',np.int32),('',np.float)])
 Rtype = np.dtype([('',np.int32),('',np.int32),('',np.float)])
 Stype = np.dtype([('',np.int32),('',np.int32),('',np.int32),('',np.int32),('',np.float)])
@@ -55,7 +55,7 @@ Stype = np.dtype([('',np.int32),('',np.int32),('',np.int32),('',np.int32),('',np
 Rbin = np.fromfile("AdS4_R_j50.bin",Rtype)
 Tbin = np.fromfile("AdS4_T_j50.bin",Ttype)
 Sbin = np.fromfile("AdS4_S_j50.bin",Stype)
-
+"""
 """
 Convert binary data to sorted numpy arrays
 """
@@ -86,10 +86,11 @@ def Ssort(Sbin):
         Sten = np.vstack((Sten,np.array([S[i][0],S[i][1],S[i][2],S[i][3],S[i][4]])))
     return Sten[1:]
 
-
+"""
 T = Tsort(Tbin)
 R = Rsort(Rbin)
 S = Ssort(Sbin)
+"""
 
 print("Data read-in complete")
 
@@ -102,58 +103,35 @@ Basis functions to be used in computing other tensors
 @jit()
 def w(n,d):
     if n<0:
-        return float(d)
+        return d
     else:
-        return float(d)+2.*n
-     
-def ks(i,d):
-    return 2.*math.sqrt(math.factorial(i)*math.factorial(i+d-1))/gamma(i + 0.5*d)
-    
-def ep(i,d):
-    f = diff(ks(i,d)*((symcos(t))**d)*jacobi(i,0.5*d -1,0.5*d,symcos(2*t)),t)
-    return lambdify(t,f)
-    
-"""
-Functions to extract the desired tensor elements from the recursion outputs.
-"""
-def Xval(i,j,k,l):
-    temp = sorted([j,k,l],reverse=True)
-    for row in range(X.shape[0]):
-        if X[row][0]==i and X[row][1]==temp[0] and X[row][2]==temp[1] and X[row][3]==temp[2]:
-            return X[row][4] 
-    print("X[%d][%d][%d][%d] not found, returning 0" % (i,j,k,l))
-    return 0
-    
+        return d+2.*n
 
-def Yval(i,j,k,l):
-    temp = sorted([k,l],reverse=True)
-    for row in range(Y.shape[0]):
-        if Y[row][0]==i and Y[row][1]==j and Y[row][2]==temp[0] and Y[row][3]==temp[1]:
-            return Y[row][4]
-    print("Y[%d][%d][%d][%d] not found, returning 0" % (i,j,temp[0],temp[1]))
-    return 0
-
-
-@jit(nopython=True,nogil=True)
+@jit(nogil=True)#nopython=True,
 def Sval(i,j,k,l):
-    if i<0 or j<0 or k<0 or l<0:
-        return 0
-    else:
-        for row in range(S.shape[0]):
-            if S[row][0]==i and S[row][1]==j and S[row][2]==k and S[row][3]==l:
-                return S[row][4]
+    #print("Asked for S(%d,%d,%d,%d)" % (i,j,k,l))
+    for row in range(S.shape[0]):
+        if S[row][0]==i and S[row][1]==j and S[row][2]==k and S[row][3]==l:
+            #print("Returning S(%d,%d,%d,%d) = %f" % (S[row][0],S[row][1],S[row][2],S[row][3],S[row][4]))
+            return S[row][4]
+    #print("Failed to find S(%d,%d,%d,%d)" % (i,j,k,l))
 
-@jit(nopython=True,nogil=True)     
+@jit(nogil=True)#nopython=True,     
 def Rval(i,j):
+    #print("Asked for R(%d,%d)" % (i,j))
     for row in range(R.shape[0]):
         if R[row][0]==i and R[row][1]==j:
-            return R[row][2]    
-    return 0
+            #print("Returning R(%d,%d) = %f" % (R[row][0],R[row][1],R[row][2]))
+            return R[row][2]
+    #print("Failed to find R(%d,%d)" % (i,j))
 
-@jit(nopython=True,nogil=True)    
+
+@jit(nogil=True)#nopython=True,    
 def Tval(i):
+    #print("Asked for T(%d)" % i)
     for row in range(T.shape[0]):
         if T[row][0]==i:
+            #print("Returning T(%d) = %f" % (T[row][0],T[row][1]))
             return T[row][1]
 
 
@@ -161,7 +139,7 @@ def Tval(i):
 These tensors will be calculated by integrals elsewhere before being used; for the test case,
 generating the values as needed is sufficient.
 """ 
-    
+""" 
 def W00(i,j,k,l,d):
     return dblquad(lambda x,y: (ks(i,d)*((math.cos(x))**d)*eval_jacobi(i,0.5*d -1,0.5*d,math.cos(2*x))) \
     * (ks(j,d)*((math.cos(x))**d)*eval_jacobi(j,0.5*d -1,0.5*d,math.cos(2*x))) * math.sin(x)*math.cos(x) \
@@ -183,7 +161,7 @@ def A(i,j,d):
 def V(i,j,d):
     return quad(lambda x: (ep(i,d)(x))*(ep(j,d)(x))*math.sin(x)*math.cos(x),0,math.pi/2.)[0]
     
-  
+"""
 ###################################################################
 ###################################################################
 
@@ -195,27 +173,60 @@ Inputs for the QP mode solver
 a0 = float(1.0)
 a1 = float(0.35)
 N = T.shape[0]
-d = 3.
+d = float(3.)
 
 
 # Initial values for each alpha_j based on (B1) of arXiv:1507.08261
 @jit(nogil=True)
-def ainits(a1):
+def inits(a0,a1):
     mu = math.log(3./(5.*a1))
-    ainits = [float(1.0e2),float(1.0e2)]
+    inits = [a0,a1]
     for i in range(2,N):
-        ainits.append(3.*math.exp(-mu*float(i))/(2.*float(i)+3.))
-    return ainits
-    
-ainits = ainits(a1)
-   
+        inits.append(3.*math.exp(-mu*float(i))/(2.*float(i)+3.))
+    return inits
+
+ainits = inits(a0,a1)
+#print(ainits)
+
+@jit()
+def makebees(seeds):
+    #print("Constructing beta")
+    beta = [float(0.)]
+    for i in range(2):
+        r = 0. ; s = 0.
+        for j in range(N):
+            if i!=j:
+                #print("Calculating r")
+                r = r + 2.*Rval(i,j)*seeds[i]*seeds[j]**2
+                #print("r =",r)
+                for k in range(N):
+                    if k+j-i<N and k!=i and j+k>=i:
+                        #print("Calculating s with (i,j,k) = (%d,%d,%d) and seeds[%d] = %f \
+                        #seeds[%d] = %f, seeds[%d] = %f" %(i,j,k,j,seeds[j],k,seeds[k],j+k-i,seeds[j+k-i]))
+                        s = s + 2.*Sval(j,k,j+k-i,i)*seeds[j]*seeds[k]*seeds[j+k-i]
+        #print("r =",r)
+        #print("s =",s)
+        #print("w =",w(i,d))
+        #print("seeds[%d] =" % i,seeds[i])
+        #print("beta[%d] value is" % i, (-2.*Tval(i)*(seeds[i])**3 - r - s)/(w(i,d)*seeds[i]))
+        beta.append((-2.*Tval(i)*(seeds[i])**3 - r - s)/(w(i,d)*seeds[i]))
+    beta = beta[1:]
+    return beta
+        
+b = makebees(ainits)
+print("beta =",b)
+
+  
+seeds = np.concatenate((b,ainits[2:]))
+print("seeds =",seeds)  
+  
 """    
 Functions for solving for QP coefficients
 """
 
 # Function that returns the input values a0, a1 when i=0,1 and returns the variable
 # being optimized when i>1
-@jit(nopython=True,nogil=True)
+@jit(nogil=True)
 def alpha(i,x):
     if i==0:
         return a0
@@ -225,21 +236,50 @@ def alpha(i,x):
         return x[i]
 
 
+
 # Use the QP equation (14) from arXiv:1507.08261
 @jit(nogil=True)
 def system(x):
-    F = np.zeros_like(x,dtype=np.float)
+    F = [0.]
     for i in range(N):
-        s = 0; r = 0
+        r = 0. ; s = 0.
         for j in range(N):
             if i!=j:
-                for k in range(N):
-                    if j+k-i<N and i<j+k and i!=k:
-                        s = s +2.*Sval(j,k,j+k-i,i)*alpha(j,x)*alpha(k,x)*alpha(j+k-i,x) 
                 r = r + 2.*Rval(i,j)*alpha(i,x)*alpha(j,x)**2
-        F[i] = 2.*Tval(i)*alpha(i,x)**3 + w(i,d)*(x[0]+float(i)*(x[1]-x[0]))*alpha(i,x) + r + s
-    return F
+                for k in range(N):
+                    if k+j-i<N and k!=i and j+k>=i:
+                        s = s + 2.*Sval(j,k,j+k-i,i)*(alpha(j,x))*(alpha(k,x))*(alpha(j+k-i,x))
+        #print("r =",r)
+        #print("s =",s)
+        F.append(2.*Tval(i)*(alpha(i,x))**3 + w(i,d)*(x[0]+float(i)*(x[1]-x[0]))*alpha(i,x) + r + s)
+    return F[1:]
+ 
+
+
     
+def ayes(i):
+    return seeds[i]
+
+@jit(nogil=True)
+def loop(b):
+    F = [0.]
+    for i in range(N):
+        r = 0. ; s = 0.
+        for j in range(N):
+            if i!=j:
+                r = r + 2.*Rval(i,j)*ayes(i)*ayes(j)**2
+                for k in range(N):
+                    if k+j-i<N and k!=i and j+k>=i:
+                        s = s + 2.*Sval(j,k,j+k-i,i)*(ayes(j))*(ayes(k))*(ayes(j+k-i))
+        #print("r =",r)
+        #print("s =",s)
+        F.append(2.*Tval(i)*(ayes(i))**3 + w(i,d)*(b[0]+float(i)*(b[1]-b[0]))*ayes(i) + r + s)
+    return F[1:]
+    
+            
+            
+ 
+
 
 # Compute the energy per mode using (5) from arXiv:1507.08261
 @jit(nogil=True)
@@ -261,19 +301,51 @@ Solve for QP coefficients
 """
 
 @jit(nogil=True)
-def solves():
+def Ksolves():
     t0 = time.process_time()
-    sol = newton_krylov(system,ainits,verbose=True,f_tol=1.0e-20,f_rtol=1.0e-10)
-    print(sol)
-    print(energy(sol))
-    t1 = time.process_time()
-    print("Calculation time =",t1-t0,"seconds")
+    sol = newton_krylov(system,seeds,verbose=True,f_rtol=1e-10)
+    #print("sol")
+    print("Krylov method \n",sol)
+    #print(energy(sol))
+    print("Krylov calculation time =",time.process_time()-t0,"seconds")
     print('\a')
     return sol
     
-sol = solves()
+Ksol = Ksolves()
+
+@jit(nogil=True)
+def Bsolves():
+    t0 = time.process_time()
+    sol = diagbroyden(system,seeds,f_rtol=1e-8)
+    print("Anderson method \n",sol)
+    print("Anderson calculation time =", time.process_time()-t0,"seconds")
+    return sol
+ 
+Bsol = Bsolves()
+   
+@jit(nogil=True)
+def Fsolves():
+    t0 = time.process_time()
+    sol = fsolve(system,seeds,xtol=1e-10)
+    print("fsolve method \n",sol)
+    print("fsolve calculation time =", time.process_time()-t0,"seconds")
+    return sol
+
+Fsol = Fsolves()    
+
+@jit(nogil=True)
+def Rsolves():
+    t0 = time.process_time()
+    sol = root(loop,seeds,method='krylov',tol=1e-10)
+    print("root with krylov method \n",sol)
+    print("root method calculation time =", time.process_time()-t0,"seconds")
+    return sol
+    
+#Rsol = Rsolves()
 
 
+
+"""
 with open("./data/AdS4QP_j50_a035.dat","w") as s:
     s.write("%d %.14e \n" % (0,a0))
     s.write("%d %.14e \n" % (1,a1))
@@ -287,7 +359,7 @@ with open("./data/AdS4QP_j50_a035E.dat","w") as f:
     for i in range(len(energy(sol))):
         f.write("%d %.14e \n" % (i,energy(sol)[i]))
     print("Wrote QP mode energies to %s" % f.name)
-
+"""
 
 
 
@@ -324,39 +396,6 @@ def f(x):
 
 ###################################################################
 ###################################################################
-
-"""
-Try to use parallel computing to cover a range of a1 values at once
-"""
-
-
- 
-
-
-
-
-"""
-if __name__ == '__main__':
-    import dispy, random
-    cluster = dispy.JobCluster(compute)
-    jobs = []
-    for i in range(10):
-        # schedule execution of 'compute' on a node (running 'dispynode')
-        # with a parameter (random number in this case)
-        job = cluster.submit(random.randint(5,20))
-        job.id = i # optionally associate an ID to job (if needed later)
-        jobs.append(job)
-    # cluster.wait() # wait for all scheduled jobs to finish
-    for job in jobs:
-        host, n = job() # waits for job to finish and returns results
-        print('%s executed job %s at %s with %s' % (host, job.id, job.start_time, n))
-        # other fields of 'job' that may be useful:
-        # print(job.stdout, job.stderr, job.exception, job.ip_addr, job.start_time, job.end_time)
-    cluster.print_status()
-"""
-
-
-
 
 
 
